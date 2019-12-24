f0fd3a695495c9d639a16e3a43ca9e08f3398537
Renan Henrique <nan.pp@hotmail.com>
2019-12-24 17:27:37 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import * as styles from './styles';
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faMoon} from '@fortawesome/free-solid-svg-icons';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      countries: [],
+      maxLoaded: 16
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then(res => res.json())
+      .then(json => this.setState({countries: json}))
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <styles.Wrapper>
+      <styles.Header>
+        <styles.Title>Where in the world?</styles.Title>
+
+        <styles.ThemeSwap>
+          <FontAwesomeIcon icon={faMoon}/>Dark Mode
+        </styles.ThemeSwap>
+        
+      </styles.Header>
+
+      <styles.searchWrapper>
+        <styles.Input placeholder="Search for a country...">
+
+        </styles.Input>
+
+        <styles.Select>
+          <option hidden={true}>Filter by region...</option>
+          <option>Africa</option>
+          <option>America</option>
+          <option>Asia</option>
+          <option>Europe</option>
+          <option>Oceania</option>
+        </styles.Select>
+      </styles.searchWrapper>
+
+      <styles.countriesWrapper>
+        { (this.state.countries.length > 0) ? this.state.countries.slice(0, this.state.maxLoaded).map((country, index) => {
+          return (
+            <styles.Card>
+              <styles.cardImg src={country.flag}/>
+
+              <styles.cardContent>
+                <styles.cardTitle>{country.name}</styles.cardTitle>
+                <styles.cardP>Population: {country.population.toLocaleString()}</styles.cardP>
+                <styles.cardP>Region: {country.region}</styles.cardP>
+                <styles.cardP>Capital: {country.capital}</styles.cardP>
+              </styles.cardContent>
+              
+            </styles.Card>
+          )
+        }) : <span>Carregando...</span> }
+        
+      </styles.countriesWrapper>
+
+      <styles.Footer>
+        <styles.loadMoreButton>
+          LOAD MORE...
+        </styles.loadMoreButton>
+        <p><a href="https://github.com/renanh3l" target="_blank">Criado por Renan Henrique</a></p>
+      </styles.Footer>
+    </styles.Wrapper>
   );
+}
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,154 @@
+import styled from 'styled-components';
+
+const lightModeBackground = "hsl(0, 0%, 98%)";
+const lightModeElements = "hsl(0, 0%, 100%)"
+const lightModeText = "hsl(200, 15%, 8%)";
+
+/*
+- Dark Blue (Dark Mode Elements): hsl(209, 23%, 22%)
+- Very Dark Blue (Dark Mode Background): hsl(207, 26%, 17%)
+- Very Dark Blue (Light Mode Text): hsl(200, 15%, 8%)
+- Dark Gray (Light Mode Input): hsl(0, 0%, 52%)
+- Very Light Gray (Light Mode Background): hsl(0, 0%, 98%)
+- White (Dark Mode Text & Light Mode Elements): hsl(0, 0%, 100%)
+*/
+
+export const Wrapper = styled.div`
+  background-color: ${lightModeBackground};
+  color: ${lightModeText};
+`
+
+export const Header = styled.header`
+  width: 100%;
+  height: 80px;
+  border-bottom: 1px solid rgba(0, 0, 0, 10%);
+  background-color: ${lightModeElements};
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 2px 0 0 rgba(0, 0, 0, 5%);
+  align-items: center;
+  justify-content: space-around;
+`
+
+export const Title = styled.h1`
+  font-family: 'Nunito Sans', sans-serif;
+  font-weight: 800;
+  font-size: 24px;
+`
+
+export const ThemeSwap = styled.div`
+  font-weight: 600;
+`
+
+export const searchWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 3em 0 0 0;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    margin: 3em 0 0 3em;
+  }
+`
+
+export const Input = styled.input`
+  height: 50px;
+  width: 400px;
+  padding-left: 20px;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 2px 2px 2px 2px rgba(0,0,0, 5%);
+
+  @media (max-width: 768px) {
+    margin-bottom: 25px;
+    max-width: 90%;
+  }
+`
+
+export const Select = styled.select`
+  height: 50px;
+  padding: 0 20px 0 20px;
+  text-indent: 5px;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 2px 2px 2px 2px rgba(0,0,0, 5%);
+
+  @media (max-width: 768px) {
+    width: 50%;
+  }
+`
+
+export const countriesWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 0 150px 0 150px;
+`
+
+export const Card = styled.div`
+  height: 400px;
+  border-radius: 3px;
+  box-shadow: 2px 2px 2px 2px rgba(0,0,0, 20%);
+  flex: 0 1 23%;
+  margin: 55px 35px 0 0;
+  max-width: 300px;
+  min-width: 300px;
+  overflow: hidden;
+
+  &:hover {
+    box-shadow: 4px 4px 15px 4px rgba(0,0,0, 30%);
+  }
+`
+
+export const cardImg = styled.img`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+`
+
+export const cardContent = styled.div`
+  margin-left: 15px;
+`
+
+export const cardTitle = styled.h1`
+  font-family: 'Nunito Sans', sans-serif;
+  font-size: 20px;
+`
+
+export const cardP = styled.p`
+
+`
+
+export const Span = styled.span`
+  padding: 15px;
+`
+
+export const Footer = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+  text-align: center;
+  
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+`
+
+export const loadMoreButton = styled.button`
+
+  border: 2px solid #4189DD;
+  border-radius: 5px;
+  padding: 15px;
+  color: #000;
+  background-color: ${lightModeElements};
+  font-family: 'Nunito Sans', sans-serif;
+  font-weight: 800;
+  font-size: 16px;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.7;
+  }
+`
